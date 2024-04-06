a27408764e65764141ee1b3382ac2e81489a8d94
CrysSilva <cryssilva1503@gmail.com>
2024-04-06 02:07:09 -0300

    meu commit

---
+++ index.js
@@ -0,0 +1,38 @@
+let pontosDeVida = 5050 
+let jogador = "Pokemon" 
+
+for (let i = pontosDeVida; i <= 1000; i++) {
+    pontosDeVida +=1
+    console.log("O " + jogador + " está com " + i + " pontos de vida")
+    console.log("Olá Herói " + jogador + " Você Está No Nível Ferro")
+} 
+ for (let i = pontosDeVida; i <= 2000; i++) {
+        pontosDeVida +=1
+        console.log("O " + jogador + " está com " + i + " pontos de vida") 
+        console.log("Olá Herói " + jogador + " Você Está No Nível Bronze")
+} for (let i = pontosDeVida; i <= 3000; i++) {
+        pontosDeVida +=1
+        console.log("O " + jogador + " está com " + i + " pontos de vida") 
+        console.log("Olá Herói " + jogador + " Você Está No Nível Prata")
+} for (let i = pontosDeVida; i <= 4000; i++) {
+        pontosDeVida +=1
+        console.log("O " + jogador + " está com " + i + " pontos de vida") 
+                console.log("Olá Herói " + jogador + " Você Está No Nível Platina")
+} for (let i = pontosDeVida; i <= 5000; i++) {
+        pontosDeVida +=1
+        console.log("O " + jogador + " está com " + i + " pontos de vida") 
+        console.log("Olá Herói " + jogador + " Você Está No Nível Diamante")
+} for (let i = pontosDeVida; i <= 6000; i++) {
+        pontosDeVida +=1
+        console.log("O " + jogador + " está com " + i + " pontos de vida") 
+        console.log("Olá Herói " + jogador + " Você Está No Nível Ascendente")
+} for (let i = pontosDeVida; i <= 7000; i++) {
+        pontosDeVida +=1
+        console.log("O " + jogador + " está com " + i + " pontos de vida") 
+        console.log("Olá Herói " + jogador + " Você Está No Nível Imortal")
+} for (let i = pontosDeVida; i <= 8000; i++) {
+        pontosDeVida +=1
+        console.log("O " + jogador + " está com " + i + " pontos de vida") 
+        console.log("Olá Herói " + jogador + " Você Está No Nível Radiante")
+}
+    console.log("Herói "  + jogador + " Você está com " + pontosDeVida + " terá que enfrentar o boss para passar de fase")
